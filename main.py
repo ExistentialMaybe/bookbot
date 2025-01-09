8d7a48aefbc954799c51715e93782322c8e3fb33
ExistentialMaybe <m.e.janca@gmail.com>
2025-01-08 22:44:23 -0500

    Final version with formatted report.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    
-    print(file_contents)
+    file_path = "books/frankenstein.txt"
+    file_text = get_book_text(file_path)
+    print(f"--- Begin report for file {file_path} ---")
+    print(f"\nThis file has {count_words(file_text)} words.")
+    print(f"\nCharacters found in file:")
+    #### alpha sort by key
+    # for char, count in sorted(count_characters(file_text).items()):
+    #     print(F"\t{char}: {count}")
+    ###sort by value in dictionary
+    for char, count in sorted(count_characters(file_text).items(), key=lambda x: x[1], reverse=True):
+        print(F"\t{char}: {count}")
+    print(f"\n--- End report for file {file_path} ---")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    return len(text.split())
+
+def count_characters(text):
+    character_dict = {}
+    for char in text.lower():
+        if char.isalpha():
+            character_dict[char] = character_dict.get(char,0) + 1
+    return character_dict
+
 if __name__ == "__main__":
     main()
